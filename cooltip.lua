8910adb0c51c290358b977b36f4d0df5d1fa456d
Lars Haugseth <github@larshaugseth.com>
2016-05-17 15:32:32 +0200

    Refactored into more readable code.

--- cooltip.lua
+++ cooltip.lua
@@ -1,1 +1,5 @@
-hooksecurefunc("GameTooltip_SetDefaultAnchor", function(self, parent) self:SetOwner(parent, "ANCHOR_CURSOR") end)
+function hook_ttAnchorCursor(self, parent)
+  self:SetOwner(parent, "ANCHOR_CURSOR")
+end
+
+hooksecurefunc("GameTooltip_SetDefaultAnchor", hook_ttAnchorCursor)
